1f5c740399879352af0332b20bad1b2d176a2a70
Titus Fortner <titusfortner@gmail.com>
2017-05-23 15:25:51 -0700

    create spec

---
+++ spec/user_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe AddressBook::Data::User do
+
+  let(:user) { AddressBook::Data::User.new }
+
+
+  it 'signup' do
+
+  end
+
+  it 'login' do
+
+  end
+
+  it 'logout' do
+
+  end
+
+end
