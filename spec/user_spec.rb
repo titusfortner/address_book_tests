57ce8d941a7558ad51a1bc141b1547989043eb43
Titus Fortner <titusfortner@gmail.com>
2017-05-25 10:00:03 -0700

    implement user spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,16 +7,26 @@ module AddressBook
 
 
     it 'signup' do
+      SignUp.visit.submit_form(user)
 
+      expect(Home.visit.signed_in_user).to eq user.email_address
     end
 
     it 'login' do
+      SignUp.visit.submit_form(user)
+      Home.new.sign_out_user
 
+      SignIn.visit.submit_form(user)
+
+      expect(Home.new.logged_in?).to eq true
     end
 
     it 'logout' do
+      SignUp.visit.submit_form(user)
 
+      Home.new.sign_out_user
+
+      expect(Home.new.logged_in?).to eq false
     end
-
   end
 end
