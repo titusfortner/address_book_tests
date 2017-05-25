a8da80b0252c5e7cef065caaba738ab5bd62bc5b
Titus Fortner <titusfortner@gmail.com>
2017-05-25 10:00:58 -0700

    implement user data

--- spec/support/data/user.rb
+++ spec/support/data/user.rb
@@ -2,8 +2,6 @@ module AddressBook
   module Data
     class User < Base
 
-      # Specify default data for each key inside block
-
       key(:email_address) { Defaults.translate :email_address }
       key(:password) { Defaults.translate :password }
 
