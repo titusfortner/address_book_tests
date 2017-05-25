dce9bc739d94de61d8b8285276f92208586ce729
Titus Fortner <titusfortner@gmail.com>
2017-05-25 10:01:39 -0700

    implement home page

--- spec/support/pages/home.rb
+++ spec/support/pages/home.rb
@@ -4,11 +4,24 @@ module AddressBook
     page_url { "#{Site.base_url}/" }
 
 
-    # Specify full Watir locator inside block
-    # element(:foo) { browser.div(id: 'foo') }
+    element(:current_user) { browser.span(data_test: 'current-user') }
+    element(:logout) { browser.a(data_test: 'sign-out') }
+    element(:sign_in) { browser.a(data_test: 'sign-in') }
+    element(:addresses) { browser.a(data_test: 'addresses') }
 
-    element(:current_user) {  }
-    element(:logout) {  }
+
+    def signed_in_user
+      return nil if sign_in.present?
+      current_user.text
+    end
+
+    def sign_out_user
+      logout.click
+    end
+
+    def logged_in?
+      current_user.present?
+    end
 
   end
 end
