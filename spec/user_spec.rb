1925125955dfd6af9c8a615436d150cb04e37941
Titus Fortner <titusfortner@gmail.com>
2017-05-25 09:59:37 -0700

    watir generate test User signup login logout

---
+++ spec/user_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module AddressBook
+  describe Data::User do
+
+    let(:user) { Data::User.new }
+
+
+    it 'signup' do
+
+    end
+
+    it 'login' do
+
+    end
+
+    it 'logout' do
+
+    end
+
+  end
+end
