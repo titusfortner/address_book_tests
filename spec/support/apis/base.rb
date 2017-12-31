74b8badc6a1515d87b8573e21ea9f2b34bde76fc
Titus Fortner <titusfortner@gmail.com>
2017-12-31 12:22:53 -0600

    tidy headers

--- spec/support/apis/base.rb
+++ spec/support/apis/base.rb
@@ -4,21 +4,19 @@ module AddressBook
   module API
     class Base < WatirApi::Base
       def self.headers
-        opt = super
-
-        opt[:cookies] = if Site.user.nil?
-                          cookies = Site.browser.cookies.to_a
-                          rt = cookies.find { |cookie| cookie[:name] == "remember_token" }
-                          abs = cookies.find { |cookie| cookie[:name] == "_address_book_session" }
-                          c = {}
-                          c[:remember_token] = rt[:value] unless rt.nil?
-                          c[:address_book_session] = abs[:value] unless abs.nil?
-                          c
-                        else
-                          {remember_token: Site.user.remember_token,
-                           address_book_session: Site.user.address_book_session}
-                        end
-        opt
+        cookies = if Site.user.nil?
+                    cookies = Site.browser.cookies.to_a
+                    rt = cookies.find { |cookie| cookie[:name] == "remember_token" }
+                    abs = cookies.find { |cookie| cookie[:name] == "_address_book_session" }
+                    c = {}
+                    c[:remember_token] = rt[:value] unless rt.nil?
+                    c[:address_book_session] = abs[:value] unless abs.nil?
+                    c
+                  else
+                    {remember_token: Site.user.remember_token,
+                     address_book_session: Site.user.address_book_session}
+                  end
+        super.merge(cookies: cookies)
       end
     end
   end
