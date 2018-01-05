9070554a09965256b46f6605b3313f8abecc8b3d
Titus Fortner <titusfortner@gmail.com>
2018-01-05 16:41:27 -0600

    these things go in base

--- spec/support/sites.rb
+++ spec/support/sites.rb
@@ -1,5 +1,17 @@
 module AddressBook
   class Site::Base
 
+    class << self
+      attr_accessor :base_url, :browser
+    end
+
+    def base_url
+      self.class.base_url
+    end
+
+    def browser
+      self.class.browser
+    end
+
   end
 end
--- spec/support/sites/address_book.rb
+++ spec/support/sites/address_book.rb
@@ -2,30 +2,6 @@ module AddressBook
   module Site
     class AddressBook
 
-      class << self
-        attr_accessor :base_url, :browser
-      end
-
-      def base_url
-        self.class.base_url
-      end
-
-      def browser
-        self.class.browser
-      end
-
-      def create_user(user = nil)
-
-      end
-
-      def create_address(address = nil)
-
-      end
-
-      def address?(address)
-
-      end
-
     end
   end
 end
